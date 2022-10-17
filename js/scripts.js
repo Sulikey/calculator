03156a3116a595d887b9fac3bedd94b758257655
sulikey <anahi.sulikey@gmail.con>
2022-10-17 15:50:36 -0700

    add function for multiply subtract and divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
-function saySomething(whatToSay) {
-    window.alert(whatToSay);
-  }
-  
-  function add(number1, number2) {
-    return number1 + number2;
-  }
+// business logic
+function add(number1, number2) {
+  return number1 + number2;
+}
 
+// business logic
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-  
-const result = add(3,5);
+// business logic
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-const outputText = "The sum is: " + result + "!";
+// business logic
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
-saySomething(outputText);
+// user interface logic 
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+const resultAdd = add(parseInt(number1), parseInt(number2));
+const resultSubtract = subtract(parseInt(number1), parseInt(number2));
+const resultMultiply = multiply(parseInt(number1), parseInt(number2));
+const resultDivide = divide(parseInt(number1), parseInt(number2));
+
+window.alert("The addition of your numbers equals: " + resultAdd + 
+" The subtraction of your numbers equals: " + resultSubtract +
+" The multiplication of your numbers equals: " + resultMultiply +
+" The division of your numbers equals: " + resultDivide);
